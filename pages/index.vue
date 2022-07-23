104efd92011a8c5cf82f4634aec9c581adf1774d
iamrosada <luisrosada2018@gmail.com>
2022-07-23 23:30:21 +0300

    the main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template>
-  <Tutorial />
+  <ContainerComponent/>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import {ContainerComponent} from '../components/ContainerComponent/container'
+
+import  '../assets/css/main.css'
 
 export default Vue.extend({
-  name: 'IndexPage'
+ components:{
+  ContainerComponent,
+ }
 })
 </script>
