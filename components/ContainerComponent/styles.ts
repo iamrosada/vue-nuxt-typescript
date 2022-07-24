f52398dce20a84cd002056302f56a8811724cc4a
iamrosada <luisrosada2018@gmail.com>
2022-07-24 14:23:02 +0300

    file for style the conatiner component

---
+++ components/ContainerComponent/styles.ts
@@ -0,0 +1,16 @@
+import styled from "vue-styled-components"
+
+export const Wrapper = styled("div")`
+display: flex;
+max-width: 1100px;
+margin: 0 auto;
+justify-content: space-between;
+padding: 90px 0px 0px 0px;
+
+
+`;
+
+export const Container = styled("div")`
+margin-top: 30px;
+
+`;
