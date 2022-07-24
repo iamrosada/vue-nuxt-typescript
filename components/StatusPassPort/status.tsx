2239c44b835c8e401105d6ade687b64355cf4067
iamrosada <luisrosada2018@gmail.com>
2022-07-24 14:24:14 +0300

    status passport

---
+++ components/StatusPassPort/status.tsx
@@ -0,0 +1,17 @@
+import Vue from "vue"
+import {ContainerStatus} from "./styles"
+import { Component, Prop } from "nuxt-property-decorator"
+import { GoodStatus } from "./good"
+
+@Component({})
+export class StatusComponent extends Vue{
+ render(){
+  return(
+  <ContainerStatus>
+    <div>
+       <GoodStatus/>
+    </div>
+
+  </ContainerStatus>)
+ }
+}
