993d015a747190dd7e50196b0f6e8e9454a1df8a
iamrosada <luisrosada2018@gmail.com>
2022-07-24 23:38:59 +0300

    created the onInputChange

--- components/SearchComponent/search.tsx
+++ components/SearchComponent/search.tsx
@@ -1,25 +1,28 @@
-import Vue, {  } from "vue"
-import {Search,ContainerInput} from "./styles"
-import { Component, Prop ,Watch} from "nuxt-property-decorator"
-
-
+import Vue from "vue";
+import { Search, ContainerInput } from "./styles";
+import { Component, Emit, Prop, Watch } from "nuxt-property-decorator";
 @Component
-export class SearchComponent extends Vue{
- text=''
- @Watch('text')
- onInput(){
-  console.log(this.text)
- }
- render(){
-  return(
-  <ContainerInput>
-   <div>
-     <Search type="text" placeholder="Digita o seu nome" v-model={this.text}/>
-     <button>Procurar</button>
-   </div>
+export class SearchComponent extends Vue {
+  @Emit()
+  onInputChange(e){
+    console.log("vvvv--->", e.target.value)
+    return e.target.value
 
-  </ContainerInput>)
- }
+  }
 
+  render() {
+    return (
+      <ContainerInput>
+        <div>
+          <Search
+            type="text"
+            placeholder="Digita o seu nome"
+            onChange={this.onInputChange}
 
+          />
+          <button>Procurar</button>
+        </div>
+      </ContainerInput>
+    );
+  }
 }
