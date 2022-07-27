ed612d17d103c3b714fd65859c3fbd0f8b6b4242
iamrosada <luisrosada2018@gmail.com>
2022-07-28 02:51:14 +0300

    feature: created new form to use the vuex

--- store/index.ts
+++ store/index.ts
@@ -1,13 +1,35 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex, { StoreOptions } from 'vuex'
+import { RootState } from '@/store/types';
+import { isToggle } from '@/store/toggle/index'
 
-
-Vue.use(Vuex)
-const store = new Vuex.Store({
+Vue.use(Vuex);
+export default () => new Vuex.Store({
+  state: () => ({
+    helloMessage: 'Hello God, YOU ARE POWERFULLY',
+    testhelo: false
+  }),
+  getters: {},
+  mutations: {},
+  actions: {},
   modules: {
-
+    isToggle
   }
-
 })
+// const store: StoreOptions<RootState> = {
+//   state: () => ({
+//     helloMessage: 'Hello God, YOU ARE POWERFULLY'
 
-export default store
+//   }),
+//   actions: {},
+//   getters: {},
+//   mutations: {},
+//   modules: {
+//     isToggle
+//   }
+
+// }
+
+// export default new Vuex.Store<RootState>(store)
+
+// export default { store }
