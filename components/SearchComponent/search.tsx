90fd13d3be6b708ff93b62432835b52f0f74b319
iamrosada <luisrosada2018@gmail.com>
2022-07-28 02:49:08 +0300

    feature: updated the search component

--- components/SearchComponent/search.tsx
+++ components/SearchComponent/search.tsx
@@ -1,48 +1,69 @@
-import Vue from "vue";
+import Vue, { ComponentOptions } from "vue";
 import { Search, ContainerInput } from "./styles";
-import store from '@/store/index'
+// import store from '@/store/modules/index'
 import { Component, Emit, Prop, Watch } from "nuxt-property-decorator";
+import { DefaultData, DefaultMethods, DefaultComputed, PropsDefinition, DefaultProps } from "vue/types/options";
 // import {namespace} from 'vuex-class'
 
 
 // import { mapMutations} from 'vuex'
 // const stu = namespace('FindStudent')
-@Component
+@Component({
+  methods:{
+    foundStudent(){
+      this.$store.dispatch('getByName')
+    }
+  },
+  computed:{
+
+  }
+})
 export class SearchComponent extends Vue {
-
-  // nameFind: string ='';
-  // listOfStudents: Array<string> = ['Apple', 'Orange', 'Banana'];
-  // @stu.Mutation
-  // public onInputChange!: (e)=>Promise<boolean>
-  // // @Emit()
-  // // onInputChange(e: { target: { value: string; }; }){
-  // //  this.nameFind = e.target.value
-  // //  return this.nameFind
-
-  // }
-  // @Emit()
-  // handleChange(){
-  // const foundStudent = this.listOfStudents.some((item)=>item.toLowerCase()===this.nameFind.toLowerCase())
-  // console.log(this.nameFind, foundStudent)
-  // }
-  // public onChange(e):void{
-  //   console.log("this.onInputChange(e)",this.onInputChange(e))
-  // }
-  get helloMesage(): string{
+  public foundStudent!: () => {};
+  get helloMessage(): string{
     return this.$store.state.helloMessage
   }
+  set helloMessage(value) {
+    this.$store.state.helloMessage = value
+  }
+  get getStudent(){
+    console.log('vvv')
+    return ''
+
+  }
+  get getActive(){
+    return this.$store.state.toggle
+  }
+  set getActive(value){
+    console.log(value)
+    this.$store.state.toggle=value
+  }
+  @Emit()
+  handleSearch(e){
+    e.preventDefault()
+    this.getStudent = e.target.value
+    this.helloMessage=e.target.value
+  }
+
+  set getStudent(value){
+    console.log('value', {t:this.$store.state.toggle})
+    this.$store.commit('SET_NAME',value)
+  }
+
   render() {
     return (
       <ContainerInput>
-        {this.helloMesage}
+        {this.helloMessage},
+
         <div>
+        {this.getActive}
           <Search
             type="text"
             placeholder="Digita o seu nome"
-            onChange={()=>{}}
+            onChange={(e)=>{this.handleSearch(e)}}
 
           />
-          <button onClick={()=>{}}>Procurar</button>
+          <button onClick={this.foundStudent}>Procurar</button>
         </div>
       </ContainerInput>
     );
