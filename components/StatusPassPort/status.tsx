a4ee1a4d9522529028c63820ca13a5021730d8b9
iamrosada <luisrosada2018@gmail.com>
2022-07-28 22:02:45 +0300

    feature: updated the status component

--- components/StatusPassPort/status.tsx
+++ components/StatusPassPort/status.tsx
@@ -1,17 +1,17 @@
-import Vue from "vue"
-import {ContainerStatus} from "./styles"
-import { Component, Prop } from "nuxt-property-decorator"
-import { GoodStatus } from "./good"
+import Vue from "vue";
+import { ContainerStatus } from "./styles";
+import { Component, Prop } from "nuxt-property-decorator";
+import { GoodStatus } from "./good";
 
 @Component({})
-export class StatusComponent extends Vue{
- render(){
-  return(
-  <ContainerStatus>
-    <div>
-       <GoodStatus/>
-    </div>
-
-  </ContainerStatus>)
- }
+export class StatusComponent extends Vue {
+  render() {
+    return (
+      <ContainerStatus>
+        <div>
+          <GoodStatus />
+        </div>
+      </ContainerStatus>
+    );
+  }
 }
