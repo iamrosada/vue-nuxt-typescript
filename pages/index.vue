f4a675d2b36791ee9671b0cb810b177dee7eef3f
iamrosada <luisrosada2018@gmail.com>
2022-07-28 13:14:30 +0300

    feature: importad the element-ui scss

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,12 @@
 import Vue from 'vue'
 import {ContainerComponent} from '../components/ContainerComponent/container'
 // import store from '@/store'
+
+// Element UI
+import ElementUI from 'element-ui'
+import '@/assets/styles/element.scss'
+Vue.use(ElementUI)
+//styles
 import  '../assets/css/main.css'
 
 export default Vue.extend({
