c26ee24f92bfd5971eaa5eb19be3da4d7f325be0
iamrosada <luisrosada2018@gmail.com>
2022-07-28 22:00:31 +0300

    feature: updated the component to show the info if status is ok

--- components/StatusPassPort/good.tsx
+++ components/StatusPassPort/good.tsx
@@ -1,28 +1,27 @@
-import Vue from "vue"
-import { Good} from "./styles"
-import { Component, Prop } from "nuxt-property-decorator"
+import Vue from "vue";
+import { Good } from "./styles";
+import { Component, Prop } from "nuxt-property-decorator";
+import { HorarioStatus } from "./horario";
 
 @Component({})
-export class GoodStatus extends Vue{
-
-  handleToggleToFalse(){
-    console.log('clicou')
-    this.$store.commit('toggleActive', false)
+export class GoodStatus extends Vue {
+  handleToggleToFalse() {
+    console.log("clicou");
+    this.$store.commit("toggleActive", false);
   }
- render(){
-  return(
- <Good>
-    <h1>
-      Está pronto!
-    </h1>
-    <p>
-      O seu Passaporte já se encontra pronto, podes levantar na Embaixada de Angola em Moscovo.
-    </p>
-
-    <button onClick={this.handleToggleToFalse}>
-      Continuar a pesquisar
-    </button>
- </Good>
-  )
- }
+  render() {
+    return (
+      <Good>
+        <h1>Está pronto!</h1>
+        <p>
+          O seu Passaporte já se encontra pronto, podes levantar na Embaixada de
+          Angola em Moscovo.
+        </p>
+        <HorarioStatus />
+        <button onClick={this.handleToggleToFalse}>
+          Continuar a pesquisar
+        </button>
+      </Good>
+    );
+  }
 }
