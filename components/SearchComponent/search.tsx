b0ceec3405d7782d10bbbd03351c664cd3c7c1bc
iamrosada <luisrosada2018@gmail.com>
2022-07-28 21:57:42 +0300

    feature: updated the search component

--- components/SearchComponent/search.tsx
+++ components/SearchComponent/search.tsx
@@ -2,56 +2,49 @@ import Vue, { ComponentOptions } from "vue";
 import { Search, ContainerInput } from "./styles";
 import { Component, Emit, Prop, Watch } from "nuxt-property-decorator";
 
-
 @Component({
-  methods:{
-
-  },
-  computed:{
-
-  }
+  methods: {},
+  computed: {},
 })
 export class SearchComponent extends Vue {
   // public foundStudent!: () => {};
   //method
-  foundStudent(){
-    console.log('apenas',this.$store.state.isToggle.toggle)
-    this.$store.dispatch('getByName')
+  foundStudent() {
+    console.log("apenas", this.$store.state.isToggle.toggle);
+    this.$store.dispatch("getByName");
   }
 
-  get helloMessage(): string{
-    return this.$store.state.helloMessage
+  get helloMessage(): string {
+    return this.$store.state.helloMessage;
   }
 
-  get getStudent(){
-
-    return ''
-
+  get getStudent() {
+    return "";
   }
 
-
-  handleSearch(e){
-    e.preventDefault()
-  //  console.log( "getter active", this.$store.getters.active)
-
-    this.getStudent = e.target.value
+  handleSearch(e) {
+    e.preventDefault();
+    if (e.target.value !== " ") {
+      this.getStudent = e.target.value;
+    }
   }
 
-  set getStudent(value){
-    console.log('value', {t:this.$store.state.toggle})
-    this.$store.commit('SET_NAME',value)
+  set getStudent(value) {
+    if (value !== "") {
+      this.$store.commit("SET_NAME", value);
+    }
   }
 
   render() {
     return (
       <ContainerInput>
-        {/* {this.helloMessage} */}
         <div>
           <Search
             type="text"
             placeholder="Digita o seu nome"
-            onChange={(e)=>{this.handleSearch(e)}}
-
+            onChange={(e) => {
+              this.handleSearch(e);
+            }}
           />
           <button onClick={this.foundStudent}>Procurar</button>
         </div>
