f2ab96ecbce657149e5097fb7ae99cc7a160546e
iamrosada <luisrosada2018@gmail.com>
2022-07-28 21:55:59 +0300

    feature: updated the header component

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -1,21 +1,24 @@
-import Vue from "vue"
-import {ContainerHeader} from "./styles"
-import { Component, Prop } from "nuxt-property-decorator"
+import Vue from "vue";
+import { ContainerHeader } from "./styles";
+import { Component, Prop } from "nuxt-property-decorator";
 
-const urlImgSme="https://www.sme.gov.ao/wp-content/themes/portalsme/assets/SME%20GOV.ao%20MAUSC.png"
-const urlImg ='https://embaixadangola.ru/wp-content/uploads/2020/10/cropped-banner-1.jpg'
+const urlImgSme =
+  "https://www.sme.gov.ao/wp-content/themes/portalsme/assets/SME%20GOV.ao%20MAUSC.png";
+const urlImg =
+  "https://embaixadangola.ru/wp-content/uploads/2020/10/cropped-banner-1.jpg";
+
 @Component({})
-export class HeaderComponent extends Vue{
- render(){
-  return(
-  <ContainerHeader>
-    <div id="embassy-container">
-      <img id="embassy"src={urlImg} alt="sim" />
-    </div>
-    <div id="container-sme">
-      <img id="sme-img" src={urlImgSme} alt="sim" />
-    </div>
-
-  </ContainerHeader>)
- }
+export class HeaderComponent extends Vue {
+  render() {
+    return (
+      <ContainerHeader>
+        <div id="embassy-container">
+          <img id="embassy" src={urlImg} alt="sim" />
+        </div>
+        <div id="container-sme">
+          <img id="sme-img" src={urlImgSme} alt="sim" />
+        </div>
+      </ContainerHeader>
+    );
+  }
 }
