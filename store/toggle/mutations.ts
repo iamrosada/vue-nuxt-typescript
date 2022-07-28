aee7e769d97abce420f8617420cacfd9d15c0d1a
iamrosada <luisrosada2018@gmail.com>
2022-07-28 21:47:31 +0300

    feature: updated the mutation

--- store/toggle/mutations.ts
+++ store/toggle/mutations.ts
@@ -16,22 +16,22 @@ export const mutations: MutationTree<ToggleState> = {
     return state.search = payload
   },
   getByName({ list, name, search, toggle }: ToggleState, payload: IStudent[]) {
-    console.log(payload, "payload")
-    const resultado = payload.filter(({ name }) => {
-      return name.toLowerCase().includes(search.toLowerCase())
-    })
+    // console.log(payload, "payload")
+    // const resultado = payload.filter(({ name }) => {
+    //   return name.toLowerCase().includes(search.toLowerCase())
+    // })
 
-    console.log(toggle, 'toggle antes')
-    console.log(resultado, 'resultado')
-    if (resultado.length > 0) {
-      toggle == !toggle;
-      list.push(resultado)
+    // console.log(toggle, 'toggle antes')
+    // console.log(resultado, 'resultado')
+    // if (resultado.length > 0) {
+    //   // toggle == !toggle;
+    //   list.push(resultado)
 
-    }
-    else {
-      toggle = toggle;
-    }
-    console.log(toggle, 'toggle depois')
+    // }
+    // // else {
+    // //   toggle = toggle;
+    // // }
+    // console.log(toggle, 'toggle depois')
   },
   toggleActive: (state, payload) => {
     state.toggle = payload
