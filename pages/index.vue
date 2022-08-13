aa21b55104bcc9d442a986bc1c964fff8a50db29
iamrosada <luisrosada2018@gmail.com>
2022-08-13 09:55:23 +0300

    feature: the last updated

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import {ContainerComponent} from '../components/ContainerComponent/container'
-//google maps
-// import * as VueGoogleMaps from 'vue2-google-maps'
-// Vue.use(VueGoogleMaps, {
-//   load: {
-//     key: 'YOUR_GOOGLE_MAPS_API_KEY_GOES_HERE',
-//     libraries: 'places',
-//   }
-// });
+
 // Element UI
 import ElementUI from 'element-ui'
 import '@/assets/styles/element.scss'
